d2fdfa82faea6016c9c3000671340559cf972116
brianbarbieri <brianbarbieri@hotmail.com>
2020-10-17 23:42:59 +0200

    added the berserk lib to communicate to API:

--- analysis.py
+++ analysis.py
@@ -1,1 +1,15 @@
-print("hello world")
+import berserk
+import datetime as dt
+
+with open("secret.csv", "r") as r:
+    data = [line.replace("\n","").split(",") for line in r.readlines()]
+    username = data[0][1]
+    token = data[1][1]
+
+session = berserk.TokenSession(token)
+client = berserk.Client(session=session)
+
+start = berserk.utils.to_millis(dt.datetime(2018, 12, 8))
+end = berserk.utils.to_millis(dt.datetime(2020, 12, 9))
+games = client.games.export_by_player(username, since=start, until=end, rated=False, max=300, perf_type="blitz", moves=False, opening=True)
+print(list(games)[0])
